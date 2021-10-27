b7fb36524116027ab27cbf91df9252851e6afb3c
mkfdev <rosy.fdev@gmail.com>
2021-10-27 13:43:42 +0900

    commit pet나이 계산하는 함수 수정

--- src/components/PetList/PetList.jsx
+++ src/components/PetList/PetList.jsx
@@ -12,30 +12,41 @@ const PetList = ({ pet }) => {
 
   const today = new Date();
 
-  const calculateAge = () => {
-    const ageL = [
-      15, 24, 28, 32, 36, 45, 50, 55, 61, 66, 72, 77, 82, 88, 93, 120,
-    ];
-    const ageM = [
-      15, 24, 28, 32, 36, 42, 47, 51, 56, 60, 65, 69, 74, 78, 83, 87,
-    ];
+  const calcPetAge = () => {
+    //소형견 나이 환산 배열
     const ageS = [
       15, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80,
     ];
+    //중형견 나이 환산 배열
+    const ageM = [
+      15, 24, 28, 32, 36, 42, 47, 51, 56, 60, 65, 69, 74, 78, 83, 87,
+    ];
+    //대형견 나이 환산 배열
+    const ageL = [
+      15, 24, 28, 32, 36, 45, 50, 55, 61, 66, 72, 77, 82, 88, 93, 120,
+    ];
+
     //현재 년도에서 출생년도 뺀 값
     let age = today.getFullYear() - birthDate.substr(0, 4);
-    let ageIndex = age - 1 < 0 ? 0 : age;
+    //배열 0부터 시작, 음수값은 0으로 처리
+    let ageIndex = age - 1 < 0 ? 0 : age - 1;
+
     //사람 나이
     setPersonAge(age + 1);
     //입력받은 size(소,중,대) 따라서 강아지 나이 환산법이 달라짐
+    //ageS, ageM, ageL 최대 16년까지만 나이 환산이 가능
+    //ageS.length = 16 보다 크거나 같으면 배열 마지막 숫자(80, 87, 120)으로 지정
     switch (size) {
       case "소":
+        ageIndex = ageIndex >= ageS.length ? ageS.length - 1 : ageIndex;
         setAge(ageS[ageIndex]);
         break;
       case "중":
+        ageIndex = ageIndex >= ageM.length ? ageM.length - 1 : ageIndex;
         setAge(ageM[ageIndex]);
         break;
       case "대":
+        ageIndex = ageIndex >= ageL.length ? ageL.length - 1 : ageIndex;
         setAge(ageL(ageIndex));
         break;
       default:
@@ -45,7 +56,7 @@ const PetList = ({ pet }) => {
 
   useEffect(() => {
     //강아지 나이 계산기
-    calculateAge();
+    calcPetAge();
   });
 
   return (
